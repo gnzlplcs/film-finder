bb7fdef4048ed1ad42f8103cc663783e3468216c
gnzlplcs <palaciosgm@outlook.com>
2022-08-17 21:40:17 -0500

    implement fetch method

--- public/script.js
+++ public/script.js
@@ -1,9 +1,21 @@
-const tmdbKey = '';
-const tmdbBaseUrl = '';
+const tmdbKey = '4f57c2756e04290058ec13af980c1610';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 const playBtn = document.getElementById('playBtn');
 
-const getGenres = () => {
+const getGenres = async () => {
+  const genreRequestEndpoint = "/genre/movie/list";
+  const requestParams = `?api_key=${tmdbKey}`
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`
+  try {
+    const response = await fetch(urlToFetch)
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const genres = jsonResponse.genres;
+      return genres;
+    }
+  } catch (error) {
 
+  }
 };
 
 const getMovies = () => {
